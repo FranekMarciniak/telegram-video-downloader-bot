0a6f201be3212b27c21a52703bd59131bba00901
Franek Marciniak <prostyfranek@protonmail.com>
2023-07-10 19:29:01 +0200

    changes

--- bot.ts
+++ bot.ts
@@ -7,6 +7,6 @@ bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
 bot.on('message::url', (ctx) => {
   const links = linkifyJS.find(ctx.message.text).map((link: {href: string}) => link.href)
-  return ctx.reply(`You sent a url: ${ctx.message.text}`);
+  return ctx.reply(`You sent a url: ${links.join(", ")}`);
 });
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
