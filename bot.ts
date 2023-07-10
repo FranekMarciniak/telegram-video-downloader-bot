22939322b353e731c44ab3826f1d61a9c089d6fc
Franek Marciniak <prostyfranek@protonmail.com>
2023-07-10 21:46:46 +0200

    download video

--- bot.ts
+++ bot.ts
@@ -7,6 +7,26 @@ bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
 bot.on('message::url', (ctx) => {
   const links = linkifyJS.find(ctx.message.text).map((link: {href: string}) => link.href)
+  const promises = links.map((link: string) => getVideoURL(link));
+  Promise.all(promises).then((data) => {
+    console.log(data);
+  })
+
   return ctx.reply(`You sent a url: ${links.join(", ")}`);
 });
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
+
+const getVideoURL = (url: string) => {
+  fetch(process.env.DOWNLOAD_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      },
+      body: JSON.stringify({url})
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    }
+  );
+}
