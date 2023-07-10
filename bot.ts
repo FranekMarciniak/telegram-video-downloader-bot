8cbd71aaccc9036723e1001af1a11ba5cba73e8c
Franek Marciniak <prostyfranek@protonmail.com>
2023-07-10 19:28:31 +0200

    test

--- bot.ts
+++ bot.ts
@@ -1,10 +1,12 @@
 import { Bot } from "./deps.deno.ts";
+import linkifyJS from "https://cdn.skypack.dev/linkifyjs?dts";
 
 export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
 bot.on('message::url', (ctx) => {
-  ctx.reply(`You sent a url: ${ctx.message.text}`);
+  const links = linkifyJS.find(ctx.message.text).map((link: {href: string}) => link.href)
+  return ctx.reply(`You sent a url: ${ctx.message.text}`);
 });
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
